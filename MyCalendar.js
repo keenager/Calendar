6946230b758c6d5bf38375d2877643ad5be5a5ab
keenager <35449911+keenager@users.noreply.github.com>
2021-04-26 02:18:28 +0900

    Add files via upload

--- MyCalendar.js
+++ MyCalendar.js
@@ -53,7 +53,7 @@ function displayCalendar(){
                 week.insertAdjacentHTML('beforeend', `
                     <td>
                         <div class='daynum'>${thisDate}</div>
-                        <div class=${j} id=${thisDate} onclick="openInputWindow(this)"></div>
+                        <div class="${j} daycontent" id="${thisDate}" onclick="openInputWindow(this)"> </div>
                     </td>
                 `);
                 
